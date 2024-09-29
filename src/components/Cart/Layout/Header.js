ec3fe38013cb2a5392546005be4348e87adf6717
Ziad Meligy <zezomelegi@gmail.com>
2024-09-29 23:51:28 +0300

    committing project

---
+++ src/components/Cart/Layout/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <React.Fragment>
+        <header>
+            <h1>ReacMeals</h1>
+            <button>Cart</button>
+        </header>
+        <div>
+            <img />
+        </div>
+    </React.Fragment>
+  )
+}
+export default Header
